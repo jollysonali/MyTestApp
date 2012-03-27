e6d79bcd5f5f1a8b2208e85e61055054bf686980
Sonali Sridhar <sonali.sridhar@gmail.com>
2012-03-27 12:25:16 -0400

    adding Foursquare getUsers

--- web.js
+++ web.js
@@ -3,7 +3,6 @@ config = require('./config');
 console.log(config);
 var Foursquare = require('node-foursquare')(config);
 
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
@@ -13,4 +12,23 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+Foursquare.Users.getUser("33", accessToken, function (error, data) {
+  var test = "Foursquare.Users.getUser(33)";
+  if(error) {
+    reportError(test, error.message);
+  }
+  else {
+    try {
+      logger.trace(sys.inspect(data));
+      assert.ok(data.user);
+      assert.equal(data.user.id, "33");
+      assert.equal(data.user.firstName, "naveen");
+      ok(test);
+    } catch (error) {
+      reportError(test, error);
+    }
+  }
+	response.send(test);
 });
