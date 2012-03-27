d912e86de62d20dbaf61a656b285d56fc4a883b9
Sonali Sridhar <sonali.sridhar@gmail.com>
2012-03-27 11:51:59 -0400

    test text

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports= {
+  "secrets" : {
+    "clientId" : process.env.FOURSQUARE_CLIENT_ID,
+    "clientSecret" : process.env.FOURSQUARE_CLIENT_SECRET,
+    "redirectUrl" : "http://falling-ice-9760.herokuapp.com/callback"
+  }
+}
+
--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+config = require('./config');
+console.log(config);
+var Foursquare = require('node-foursquare')(config);
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('hey hey!');
 });
 
 var port = process.env.PORT || 3000;
